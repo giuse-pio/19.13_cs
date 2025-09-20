1198bb1144416fffab65fe4a777a9db3b5412810
giupe <giupe@MSI>
2025-09-20 16:49:28 +0200

    Add project files.

---
+++ 19.13/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _19._13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numeri = new List<int> { 4, 2, 1, 45, 15, 28, 19, 3, 2, 5, 2 };
+            int x;
+            while (!int.TryParse(Console.ReadLine(), out x))
+            {
+                Console.WriteLine("errore");  
+            }
+            int count = 0;
+            foreach (int n in numeri)
+            {
+                if(n==x)
+                    count++;
+            }
+
+            if (count == 1)
+                Console.WriteLine($"il numero {x} è stato trovato {count} volta");
+            else
+                Console.WriteLine($" il numero {x} è stato trovato {count} volte ");
+        }
+    }
+}
